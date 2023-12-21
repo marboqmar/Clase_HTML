a1cc197bf941dd3f988757fb1c3a75bbbeba7e8d
marboqmar <martamartinezboquer@gmail.com>
2023-12-21 10:19:53 +0100

    Modified pokemon object to add properties id and img

--- my-pokedex/main.js
+++ my-pokedex/main.js
@@ -2,15 +2,21 @@ import axios from 'axios';
 import { capitalize } from 'lodash';
 import { apiUrl, getImage } from "./src/utils.js";
 
+let pokemonModifiedData
+
 axios.get(apiUrl).then((body) => {
-   let pokemonData = body.data.results;
+   let pokemonOriginalData = body.data.results;
    let html = '';
 
-   pokemonData.forEach((pokemon, index) => {
-      html = html + `<div class="pokemon"><a href="/src/views/details/?pokemonId=${pokemon.id}"><p class="pokemonName">${capitalize(pokemon.name)}</p><img src="${getImage(index + 1)}"></div>`
+   pokemonOriginalData.forEach((pokemon, index) => {
+      pokemonModifiedData = {...pokemon, id: index + 1, img: getImage(index+1)};
+
+      html = html + `<div class="pokemon"><a href="/src/pokemonDetails/?pokemonId=${pokemonModifiedData.id}"><p class="pokemonName">${capitalize(pokemonModifiedData.name)}</p><img src="${pokemonModifiedData.img}"></div>`
    });
 
    document.querySelector('.pokemonDisplay').innerHTML = html;
 });
+
+
 
 
