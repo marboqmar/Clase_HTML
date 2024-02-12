4d19f19495f8982cb51fe3e7c7d4d23e5034ad63
marboqmar <martamartinezboquer@gmail.com>
2024-02-12 14:47:04 +0100

    completed functionality, and substituted ifs for switch

--- calculator-typescript/src/App.tsx
+++ calculator-typescript/src/App.tsx
@@ -3,43 +3,96 @@ import './App.css';
 
 const actions: (string | number)[] = ['C', '+-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
 
+
 function App() {
-  const [value, setValue] = useState<string>('0');
+    const [value, setValue] = useState<string>('0');
+    const [operation, setOperation] = useState<string>('');
 
-  const handleClick = (actionClicked: any) => {
+    const calculate = () => {
+        const numbers: string[] = value.split(operation);
+        const num1: number = Number(numbers[0]);
+        const num2: number = numbers[1] === '' || typeof numbers[1] === 'undefined' ? num1 : Number(numbers[1]);
+        let result: number = 0;
 
-      if (actionClicked === '.' && !value.includes('.')) {
-          setValue(value + actionClicked);
-          return;
-      }
+        switch(operation){
+            case 'x': {
+                result = num1 * num2;
+                break;
+            }
+            case '+': {
+                result = num1 + num2;
+                break;
+            }
+            case '-': {
+                result = num1 - num2;
+                break;
+            }
+            default: {
+                result = num1 / num2;
+                break;
+            }
+        }
 
-      if (actionClicked === 'C') {
-          setValue('0');
-          return;
-      }
+        setValue(result.toString())
+    }
 
-      if (actionClicked === '+-') {
-          setValue((Number(value) * -1).toString());
-          return;
-      }
+    const handleClick = (actionClicked: any) => {
+        switch(actionClicked){
+            case 'C': {
+                setValue('0');
+                return;
+            }
+            case '+-': {
+                setValue((Number(value) * -1).toString());
+                return;
+            }
+            case '%': {
+                setValue((Number(value) / 100).toString());
+                return;
+            }
+            case '.': {
+                if (value.includes('.')) {
+                    return;
+                } else {
+                    setValue(value + actionClicked);
+                    return;
+                }
+            }
+            case '=': {
+                setOperation('=')
 
-      if (actionClicked === '%') {
-          setValue((Number(value) / 100).toString());
-          return;
-      }
+                if (typeof operation === 'undefined' || operation === '=') {
+                    return;
+                }
 
-      if (typeof actionClicked !== 'number') {
-          return;
-      } else {
-          if (value === '0') {
-              setValue(actionClicked.toString());
-          } else {
-              setValue(value + actionClicked.toString())
-          }
-      }
-  }
+                calculate();
+                return;
+            }
+        }
 
-  return (
+        if (typeof actionClicked !== 'number') {
+            const lastChar: string = value.slice(-1);
+            setOperation(actionClicked);
+
+            if (lastChar === operation) {
+                const newValue: string = value.replace(lastChar, actionClicked);
+                setValue(newValue);
+                return;
+            }
+
+            setValue(value + actionClicked);
+
+            return;
+        }
+
+        if (value === '0') {
+            setValue(actionClicked.toString());
+        } else {
+            setValue(value + actionClicked);
+        }
+    }
+
+    return (
       <>
         <div className={"mainContainer"}>
           <div className={"display"}>{value}</div>
@@ -50,7 +103,7 @@ function App() {
           </div>
         </div>
       </>
-  );
+    );
 }
 
 export default App;
